fb5df793be75adbe70b1c4e585d9fa020cf0a560
yf_ming <yf_ming@csu.edu.cn>
2024-01-08 16:47:30 +0800

    submit

---
+++ test.py
@@ -0,0 +1,20 @@
+'''
+Author       : yf_Ming && yf_Ming@csu.edu.cn
+Date         : 2023-10-20 11:45:43
+LastEditors  : yf_Ming@hpc_web yf_Ming@csu.edu.cn
+LastEditTime : 2023-10-20 11:46:14
+FilePath     : /Skorch/ProtienDT/bio-transformer/test.py
+Copyright (c) 2023, All Rights Reserved.
+'''
+'''
+Author       : yf_Ming && yf_Ming@csu.edu.cn
+Date         : 2023-10-20 11:43:06
+LastEditors  : yf_Ming@hpc_web yf_Ming@csu.edu.cn
+LastEditTime : 2023-10-20 11:44:34
+FilePath     : /Skorch/ProtienDT/bio-transformer/test.py
+Copyright (c) 2023, All Rights Reserved.
+'''
+
+import torchmetrics
+
+torchmetrics.Accuracy()
